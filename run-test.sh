1f6920b19c61e029fb4cf620a97fb923ba35ef27
salmanali515253 <salmanali515253@gmail.com>
2022-11-25 17:10:23 +0500

    Test automation test

---
+++ run-test.sh
@@ -0,0 +1,8 @@
+trcli \
+   -h "http://127.0.0.1:7777/testrail" \
+   -u "ssalmanburhan@gmail.com" \
+   -p "salmanali03463816762" \
+   --project "markdown Test" \
+   parse_junit \
+   --title "Automated test" \
+   -f "ci_test.xml"
