57a2b10d4a1ab70844c3326cd3ddf379d5cd8b5f
salmanali515253 <salmanali515253@gmail.com>
2022-11-29 15:40:07 +0500

    changes in phpunit

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,4 @@
+vendor/bin/phpunit
 trcli -y \
    -h "http://127.0.0.1:7777/testrail" \
    -u "ssalmanburhan@gmail.com" \
@@ -5,5 +6,4 @@ trcli -y \
    --project "Trcli Test" \
    parse_junit \
    --title "Automated test 3" \
-   --suite-id 2 \
    -f "ci_test.xml"
